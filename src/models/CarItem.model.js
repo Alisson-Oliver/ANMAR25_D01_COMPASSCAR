2eebc55b63e0068fa135b99ec7a3dfb3b68539cb
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-03 17:24:38 -0300

    feat(models): add car item model

---
+++ src/models/CarItem.model.js
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.config.js";
+
+const CarItem = sequelize.define(
+  "CarItem",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "cars_items",
+    timestamps: false,
+  },
+);
+
+export default CarItem;
