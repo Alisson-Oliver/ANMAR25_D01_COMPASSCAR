ddd470fef86a33c923e7adb79ab187378ad194d7
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-04 15:19:31 -0300

    feat(repository): add createCar function and findBylate function

--- src/repositories/CarRepository.js
+++ src/repositories/CarRepository.js
@@ -1,31 +1,12 @@
 import Car from "../models/Car.model.js";
-import CarItem from "../models/CarItem.model.js";
 
 class CarRepository {
-  static async findAll() {
-    return await Car.findAll();
+  static async findByPlate(plate) {
+    return await Car.findOne({ where: { plate } });
   }
 
-  static async findById(id) {
-    return await Car.findByPk(id, {
-      include: {
-        model: CarItem,
-        as: "items",
-        attributes: ["name"],
-      },
-    });
-  }
-
-  static async create(carData) {
+  static async createCar(carData) {
     return await Car.create(carData);
-  }
-
-  static async update(id, updateData) {
-    return await Car.update(updateData, { where: { id } });
-  }
-
-  static async delete(id) {
-    return await Car.destroy({ where: { id } });
   }
 }
 
