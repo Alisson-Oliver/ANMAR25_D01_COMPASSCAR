2bde9ba4535680bbe9b2359f975737fefa386076
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-05 00:40:56 -0300

    feat(route): add GET /cars  endpoint

--- src/routes/car.routes.js
+++ src/routes/car.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import CarController from "../controllers/CarController.js";
 import { validateAddCar } from "../middlewares/validation.middleware.js";
+
 const router = Router();
 
 router.post("/cars", validateAddCar, CarController.create);
+router.get("/cars", CarController.getAll);
 
 export default router;
