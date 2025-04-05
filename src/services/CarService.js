929c197c7f284601021de1f6f60352d4c26c892e
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-05 00:35:16 -0300

    feat(service): add findCarById logic

--- src/services/CarService.js
+++ src/services/CarService.js
@@ -14,6 +14,25 @@ class CarService {
   static async findAll() {
     return await CarRepository.findAll();
   }
+
+  static async findCarById(id) {
+    const existingCar = await CarRepository.findById(id);
+
+    if (!existingCar) {
+      throw new Error("car not found");
+    }
+
+    const response = {
+      id: existingCar.id,
+      brand: existingCar.brand,
+      model: existingCar.model,
+      year: existingCar.year,
+      plate: existingCar.plate,
+      created_at: existingCar.created_at,
+      items: existingCar.items.map((item) => item.name),
+    };
+    return response;
+  }
 }
 
 export default CarService;
