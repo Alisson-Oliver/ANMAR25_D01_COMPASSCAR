7a4ce42ff81792cac89f216faafceb918fd5d8b4
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-05 00:39:17 -0300

    feat(config): add carItem route

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import carRoutes from "../routes/car.routes.js";
+import carItemRoutes from "../routes/car-item.routes.js";
 import errorHandler from "../middlewares/error-handler.middleware.js";
 
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1", carRoutes);
+app.use("/api/v1", carRoutes, carItemRoutes);
 
 app.use(errorHandler);
 
