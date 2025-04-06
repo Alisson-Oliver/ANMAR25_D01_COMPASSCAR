29c09b499198856535830669f950bd089da7d391
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-06 00:34:31 -0300

    feat(controller): add updateCar handler

--- src/controllers/CarController.js
+++ src/controllers/CarController.js
@@ -39,6 +39,26 @@ class CarController {
       return res.status(500).json({ error: error.message });
     }
   }
+
+  static async updateCar(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id || isNaN(id)) {
+        return res.status(400).json({ error: "id is required" });
+      }
+
+      await CarService.updateCar(id, req.body);
+      return res.status(204).send();
+    } catch (error) {
+      if (error.message === "car not found") {
+        return res.status(404).json({ error: [error.message] });
+      } else if (error.message === "car already registered") {
+        return res.status(409).json({ error: [error.message] });
+      }
+      return res.status(500).json({ error: error.message });
+    }
+  }
 }
 
 export default CarController;
